a639d0b9b3cabaa695068fdc82938c2ee26fd88e
K-S-A <atillas@ukr.net>
2015-06-06 01:01:21 +0300

    Fix #compare and update Gemfile.lock.

--- lib/rg_rack.rb
+++ lib/rg_rack.rb
@@ -9,36 +9,44 @@ class Racker
 
   def initialize(env)
     @request = Rack::Request.new(env)
+    @game = RgCodebreaker::Game.new
   end
 
   def response
     case @request.path
-    when "/"
-      Rack::Response.new(render("index.html.erb"))
+    when "/" then Rack::Response.new(render("index.html.erb"))
     when "/guess"
       Rack::Response.new do |response|
-        @game = YAML::load(@request.cookies['var'])
+        @game = load('var')
         response.set_cookie("guess_log", @request.cookies["guess_log"] + "*" + @game.compare(@request.params["guess"]))
-        response.set_cookie('var', YAML::dump(@game))
+        response.set_cookie('var', dump(@game))
         response.redirect("/")
       end
     when "/start"
-      @game = RgCodebreaker::Game.new
       @game.start
       Rack::Response.new do |response|
         response.set_cookie("guess_log", '*')
         response.set_cookie("hint", '')
-        response.set_cookie("var", YAML::dump(@game))
+        response.set_cookie('var', dump(@game))
         response.redirect("/")
       end
     when '/hint'
       @game = YAML::load(@request.cookies['var'])
       Rack::Response.new do |response|
         response.set_cookie("hint", @game.compare('hint'))
+        response.set_cookie('var', dump(@game))
         response.redirect("/")
       end
     else Rack::Response.new("Not Found", 404)
     end
+  end
+
+  def load(cookie)
+    YAML::load(@request.cookies[cookie])
+  end
+
+  def dump(name)
+    YAML::dump(name)
   end
 
   def render(template)
