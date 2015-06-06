78fd03f17db1209eeb5240f304ae984e26981995
K-S-A <atillas@ukr.net>
2015-06-06 17:08:24 +0300

    Refactor rg_rack.rb and view. Remove #load.

--- lib/rg_rack.rb
+++ lib/rg_rack.rb
@@ -1,4 +1,4 @@
-require "erb"
+require 'erb'
 require 'rg_codebreaker'
 require 'yaml'
 
@@ -9,17 +9,56 @@ class Racker
 
   def initialize(env)
     @request = Rack::Request.new(env)
-    @game = RgCodebreaker::Game.new
+    @game = @request.cookies['var'] ? YAML::load(@request.cookies['var']) : RgCodebreaker::Game.new
   end
 
   def response
     case @request.path
-    when "/" then Rack::Response.new(render("index.html.erb"))
-    when "/guess" then guess
-    when "/start" then start
-    when '/hint' then hint
-    else Rack::Response.new("Not Found", 404)
+      when '/'        then Rack::Response.new(render("index.html.erb"))
+      when '/start'   then start
+      when '/guess'   then guess
+      when '/hint'        then hint
+      else                  Rack::Response.new("Not Found", 404)
     end
+  end
+
+  def start
+    @game.start
+    Rack::Response.new do |response|
+      %w(hint guess_log).each { |cookie| response.delete_cookie(cookie) }
+      dump(response)
+    end
+  end
+
+  def guess
+    Rack::Response.new do |response|
+      response.set_cookie('guess_log', "#{(@request.cookies['guess_log'] || '')}*#{@game.compare(@request.params['guess'])}")
+      dump(response)
+    end
+  end
+
+  def hint
+    Rack::Response.new do |response|
+      response.set_cookie('hint', @game.compare('hint'))
+      dump(response)
+    end
+  end
+
+  def guess_log
+    @request.cookies['guess_log'].split('*').reject{|i| i == 'invalid' || i.empty?}.reverse if @request.cookies['guess_log']
+  end
+
+  def flash
+      'Invalid guess, try again!' if @request.cookies['guess_log'] && @request.cookies['guess_log'].split('*').last == 'invalid'
+  end
+
+  def help
+    @request.cookies['hint']
+  end
+
+  def dump(response)
+    response.set_cookie('var', YAML::dump(@game))
+    response.redirect('/')
   end
 
   def render(template)
@@ -27,57 +66,4 @@ class Racker
     ERB.new(File.read(path)).result(binding)
   end
 
-  def load(cookie)
-    YAML::load(@request.cookies[cookie])
-  end
-
-  def dump(name)
-    YAML::dump(name)
-  end
-
-  def guess_log
-    @request.cookies["guess_log"].split('*').reject{|i| i == 'invalid' || i.empty?}.reverse || [] if @request.cookies["guess_log"]
-  end
-
-  def flash
-    if @request.cookies["guess_log"]
-      'Invalid guess, try again!' if @request.cookies["guess_log"].split('*').last == 'invalid'
-    end
-  end
-
-  def attempt
-    YAML::load(@request.cookies['var']).attempts if @request.cookies['var']
-  end
-
-  def help
-    @request.cookies['hint']
-  end
-
-  def start
-    @game.start
-    Rack::Response.new do |response|
-      response.set_cookie("guess_log", '*')
-      response.set_cookie("hint", '')
-      response.set_cookie('var', dump(@game))
-      response.redirect("/")
-    end
-  end
-
-  def guess
-    Rack::Response.new do |response|
-      @game = load('var')
-      response.set_cookie("guess_log", @request.cookies["guess_log"] + "*" + @game.compare(@request.params["guess"]))
-      response.set_cookie('var', dump(@game))
-      response.redirect("/")
-    end
-  end
-
-  def hint
-    @game = YAML::load(@request.cookies['var'])
-    Rack::Response.new do |response|
-      response.set_cookie("hint", @game.compare('hint'))
-      response.set_cookie('var', dump(@game))
-      response.redirect("/")
-    end
-  end
 end
